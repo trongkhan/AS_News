cda91c68079d4730955387ec7002051529010daf
trongkhan <trongkhanghqv@gmail.com>
2025-04-20 16:38:07 +0700

    Initial Commit

---
+++ AS News/AS_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AS_NewsApp.swift
+//  AS News
+//
+//  Created by Trọng Khang Hồ on 20/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AS_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
